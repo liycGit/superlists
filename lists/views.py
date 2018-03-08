cf743ddbdb6d06b1733c711a5fb4a75f4c1cb424
liycGit <36752041+liycGit@users.noreply.github.com>
2018-03-08 15:49:38 +0800

    第五章5.9学习完成

--- lists/views.py
+++ lists/views.py
@@ -24,10 +24,19 @@ def home_page(request):
     return render(request, 'home.html',{
 		'new_item_text':request.POST.get('item_text',''),
 	})
-"""
+
 #5.7节
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
     return render(request, 'home.html')
+"""
+#5.8节
+def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
